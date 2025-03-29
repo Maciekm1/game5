87b174649ecdcf9fa701295d72266149f9d307d1
George Howarth <george@pyesmeadow.com>
2025-03-29 15:08:48 +0000

    Resolve merge conflicts

--- scenes/main.gd
+++ scenes/main.gd
@@ -11,14 +11,17 @@ func _on_player_hit() -> void:
 func game_over() -> void:
 	$ScoreTimer.stop()
 	$MobTimer.stop()
-	
-func _ready():
-	new_game()
+	$HUD.show_game_over()
 	
 func new_game():
+	get_tree().call_group("mobs", "queue_free")
+	
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
 
 
 func _on_mob_timer_timeout() -> void:
@@ -49,8 +52,13 @@ func _on_mob_timer_timeout() -> void:
 
 func _on_score_timer_timeout() -> void:
 	score += 1
+	$HUD.update_score(score)
 
 
 func _on_start_timer_timeout() -> void:
 	$MobTimer.start()
 	$ScoreTimer.start()
+
+
+func _on_hud_start_game() -> void:
+	new_game() # Replace with function body.
