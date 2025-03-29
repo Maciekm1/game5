df1ad6f622453a8d45b62e29a77555bdea93b515
Maciek Zaweracz <maciekm1pro@gmail.com>
2025-03-29 15:10:55 +0000

    implement enemy spawn rate

--- scenes/globals.gd
+++ scenes/globals.gd
@@ -1,1 +1,13 @@
 extends Node
+
+var player_speed: int
+var enemy_speed_min: int
+var enemy_speed_max: int
+var enemy_spawn_rate: int
+
+func _ready() -> void:
+	enemy_spawn_rate = 1
+	player_speed = 400
+	enemy_speed_min = 250
+	enemy_speed_max = 500
+	
